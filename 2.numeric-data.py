ea651e18644453d217a093faece52e2a3e7101b6
Mikkung <112644725+Mikkung@users.noreply.github.com>
2025-01-13 19:39:03 +0700

    Add files via upload

---
+++ 2.numeric-data.py
@@ -0,0 +1,14 @@
+print("Python has three numeric types: int, float, and complex")
+myValue=1
+
+myValue=3.14
+
+myValue=5j
+
+myValue=True
+
+myValue=False
+
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
